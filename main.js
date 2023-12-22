c968e388b264bfadd23aedc3d4d0466ec7bfdfa9
PierluigiPapa <pierluigipapa9@gmail.com>
2023-12-22 15:56:51 +0100

    feat: add const about full price and discounted price

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 //Variabile per chiedere quanti i km da percorrere
 let km= prompt("Quanti km dovrai percorrere?");
-console.log(km);
 
 //Variabile per chiedere l'età
 let età= prompt("Quanti anni hai?");
-console.log (età);
 
+
+console.log (km, età);
+
+//Costante per prezzo intero del biglietto
+const prezzoIntero = km * 0.21;
