35b3df9d693f0a80eb2b4f8c2a3f687bd7b197ca
PierluigiPapa <pierluigipapa9@gmail.com>
2023-12-22 15:08:13 +0100

    feat: let about age and km

---
+++ main.js
@@ -0,0 +1,8 @@
+//Variabile per chiedere quanti i km da percorrere
+let km= prompt("Quanti km dovrai percorrere?");
+console.log(km);
+
+//Variabile per chiedere l'età
+let età= prompt("Quanti anni hai?");
+console.log (età);
+
