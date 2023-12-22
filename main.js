829d5158ffca419d62eeb81489885b9a51f22fd0
PierluigiPapa <pierluigipapa9@gmail.com>
2023-12-22 16:41:29 +0100

    fix: ajdust main.js

--- main.js
+++ main.js
@@ -17,14 +17,26 @@ if  (età < 18) {
     let prezzoUnder18 = prezzoIntero - (prezzoIntero * 0.2);
     let prezzoUnder18Decimale = prezzoUnder18.toFixed(2);
     console.log (prezzoUnder18Decimale);
-
+    document.writeln(`<h1> Il prezzo della tratta Brindisi-Bari è di ${(prezzoUnder18Decimale)}€</h1>`); 
+    
+} 
 
 // Applicare sconto del 40% per Over65 
 // (Calcolo della percentuale prezzointero * 40 / 100 = Sconto)
 // (Prezzo intero - Sconto = Biglietto scontato)
-} else if (età >= 65) {
-    console.log ("Hai diritto allo sconto del 20%");
+
+else if (età >= 65) {
+    console.log ("Hai diritto allo sconto del 40%");
     let prezzoOver65 = prezzoIntero - (prezzoIntero * 0.4);
     let prezzoOver65Decimale = prezzoOver65.toFixed(2);
     console.log (prezzoOver65Decimale);
+    document.writeln(`<h1> Il prezzo della tratta Brindisi-Bari è di ${(prezzoOver65Decimale)}€ </h1>`); 
 }
+
+// Nessun sconto se tra Over18 e Under65
+else {
+    console.log ("Non hai diritto ad alcun sconto");
+    console.log (prezzoIntero);
+    document.writeln(`<h1>Il prezzo della tratta Brindisi-Bari è di ${(prezzoIntero)}€ </h1>`); 
+}
+
